7f68f7f0d3a69bf7a45404affec0cabf2ed04d21
whatever413y <angel.tardaguela@gmail.com>
2024-09-06 14:46:43 +0800

    feat: Added new Game Menu scene and updated scene navigation
    - Added a new Game Menu scene to the project
    - Updated the SceneNavigationManager script to include functions for navigating to the Game Menu scene

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameMenuController : MonoBehaviour
+public class SettingsController : MonoBehaviour
 {
     public GameObject settingsPanel;
     public Button settingsButton;
