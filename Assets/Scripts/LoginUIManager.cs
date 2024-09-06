311bdfae36f259b1891d89ddbca8abd4b6c38fe3
jiv <jivayydelacruz@gmail.com>
2024-09-06 15:23:28 +0800

    feat: Add login and register UI implementation

---
+++ Assets/Scripts/LoginUIManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginUIManager : MonoBehaviour
+{
+    public InputField emailInputField;
+    public InputField passwordInputField;
+    public Button loginButton;
+    public Button registerButton;
+    void Start()
+    {
+        loginButton.onClick.AddListener(OnLoginButtonClick);
+        registerButton.onClick.AddListener(OnRegisterButtonClick);
+    }
+
+    void OnLoginbuttonClick()
+    {
+        string email = emailInputField.text;
+        string password = passwordInputField.text;
+
+        if (IsValidEmail(email) && IsValidPassword(password))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        else
+        {
+            Debug.Log("Invalid email or password");
+        }
+    }
+
+    void OnRegisterButtonClick()
+    {
+        SceneManager.LoadScene("RegisterScene");
+    }
+
+    bool IsValidEmail(string email)
+    {
+        return email.Contains("@") && email.Contains(".");
+    }
+
+    bool IsValidPassword(string password)
+    {
+        return password.Length >= 8;
+    }
+}
