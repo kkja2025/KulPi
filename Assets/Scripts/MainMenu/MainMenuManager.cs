7c9773e1df5128a2b25466eefbe7b76e742f6c1d
whatever413y <angel.tardaguela@gmail.com>
2024-09-07 20:09:48 +0800

    refactor: Add MainMenu and MainMenuManager scripts
    - Add MainMenu.cs script to handle the functionality of the main menu panel.
    - Add MainMenuManager.cs script to manage the main menu and handle user authentication.

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private bool initialized = false;
+    private bool eventsInitialized = false;
+    private static MainMenuManager singleton = null;
+
+    public static MainMenuManager Singleton
+    {
+        get
+        {
+            if (singleton == null)
+            {
+                singleton = FindFirstObjectByType<MainMenuManager>();
+                singleton.Initialize();
+            }
+            return singleton;
+        }
+    }
+
+    private void Initialize()
+    {
+        if (initialized) { return; }
+        initialized = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (singleton == this)
+        {
+            singleton = null;
+        }
+    }
+
+    private void Awake()
+    {
+        Application.runInBackground = true;
+        StartClientService();
+    }
+
+    public async void StartClientService()
+        {
+            PanelManager.CloseAll();
+            // PanelManager.GetSingleton("loading").Open();
+            try
+            {
+                if (UnityServices.State != ServicesInitializationState.Initialized)
+                {
+                    var options = new InitializationOptions();
+                    options.SetProfile("default_profile");
+                    await UnityServices.InitializeAsync();
+                }
+
+                if (!eventsInitialized)
+                {
+                    SetupEvents();
+                }
+
+                if (AuthenticationService.Instance.SessionTokenExists)
+                {
+                    PanelManager.GetSingleton("main").Open();
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.Log(exception);
+                // ShowError(ErrorMenu.Action.StartService, "Failed to connect to the network. client service", "Retry");
+            }
+        }
+
+        private void SetupEvents()
+        {
+            eventsInitialized = true;
+            AuthenticationService.Instance.Expired += () =>
+            {
+                SceneManager.LoadScene("Login");
+            };
+        }
+
+        public void SignOut()
+        {
+            AuthenticationService.Instance.SignOut();
+            SceneManager.LoadScene("Login");
+        }
+    }
