310fbb13e2a6d46c29574a14cf79db4146acc79f
KewlWhip <78395423+KertSolas@users.noreply.github.com>
2024-09-10 21:36:31 +0800

    feat: added encyclopedia panels

--- Assets/Scripts/Encyclopedia/ModalManager.cs
+++ Assets/Scripts/Encyclopedia/ModalManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ModalManager : MonoBehaviour
 {
     public GameObject modalPanel;
+    public GameObject[] encyclopediaPanels;
 
     public void ToggleModal()
     {
@@ -14,5 +15,18 @@ public class ModalManager : MonoBehaviour
     public void CloseModal()
     {
         modalPanel.SetActive(false);
+    }
+
+    public void ShowPanel(int buttonIndex)
+    {
+        for (int i=0; i < encyclopediaPanels.Length; i++)
+        {
+            encyclopediaPanels[i].SetActive(false);
+        }
+
+        if (buttonIndex >= 0 && buttonIndex < encyclopediaPanels.Length)
+        {
+            encyclopediaPanels[buttonIndex].SetActive(true);
+        }
     }
 }
