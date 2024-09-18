4f786b13e2d99cfb70a81a61832b8f0e8c8a8f26
Kyle Dominic Lomigo <kyledominic.lomigo-21@cpu.edu.ph>
2024-09-19 00:00:03 +0800

    refarctor: modified HUDs and pause and main menu script

--- Assets/Scripts/Game/Menus/HUDMenu.cs
+++ Assets/Scripts/Game/Menus/HUDMenu.cs
@@ -14,7 +14,6 @@ public class HUDMenu : Panel
         {
             return;
         }
-        pauseButton.onClick.AddListener(OpenPause);
         inventoryButton.onClick.AddListener(OpenInventory);
         encyclopediaButton.onClick.AddListener(OpenEncyclopedia);
         base.Initialize();
@@ -23,32 +22,6 @@ public class HUDMenu : Panel
     public override void Open()
     {
         base.Open();
-    }
-
-    private async void OpenPause()
-    {
-        PanelManager.GetSingleton("pause").Open();
-
-        EncyclopediaItem diwataItem = EncyclopediaItem.Figures_Chapter1_Diwata();
-        await EncyclopediaManager.Singleton.AddItem(diwataItem);
-
-        EncyclopediaItem sacredGrove = EncyclopediaItem.Events_Chapter1_Sacred_Grove();
-        await EncyclopediaManager.Singleton.AddItem(sacredGrove);
-
-        EncyclopediaItem cursedLandOfSugbu = EncyclopediaItem.Events_Chapter1_Cursed_Land_of_Sugbu();
-        await EncyclopediaManager.Singleton.AddItem(cursedLandOfSugbu);
-
-        EncyclopediaItem filipinoMedicine = EncyclopediaItem.PracticesAndTraditions_Chapter1_Traditional_Filipino_Medicine();
-        await EncyclopediaManager.Singleton.AddItem(filipinoMedicine);
-
-        EncyclopediaItem powersAndFilipinoSpirituality = EncyclopediaItem.PracticesAndTraditions_Chapter1_Powers_and_Filipino_Spirituality();
-        await EncyclopediaManager.Singleton.AddItem(powersAndFilipinoSpirituality);
-
-        EncyclopediaItem tikbalang = EncyclopediaItem.MythologyAndFolklore_Chapter1_Mythical_Creatures_Tikbalang();
-        await EncyclopediaManager.Singleton.AddItem(tikbalang);
-
-        EncyclopediaItem sigbin = EncyclopediaItem.MythologyAndFolklore_Chapter1_Mythical_Creatures_Sigbin();
-        await EncyclopediaManager.Singleton.AddItem(sigbin);
     }
 
     private void OpenInventory()
---
+++ Assets/Scripts/Game/Menus/PauseManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
