65b96ba57fe5f0aefe7ec73d52a3ba712cf3ecce
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2024-10-01 15:46:55 +0800

    refactored spawner

--- Assets/Scripts/Combat_Scripts/Combat_Spawner_Scripts/SpawnerBossDiwata.cs
+++ Assets/Scripts/Combat_Scripts/Combat_Spawner_Scripts/SpawnerBossDiwata.cs
@@ -4,27 +4,26 @@ using UnityEngine.UI;
 
 public class SpawnerBossDiwata : SpawnerSigbinTikbalang
 {
+    private BattleManagerDiwata battleManager;
     public override void OnButtonClicked(GameObject spawnButton)
     {
-        AudioManager.Singleton.PlaySwordSoundEffect(clickCount);
-        clickCount++;
-        Destroy(spawnButton);
-        currentSpawns.Remove(spawnButton);
+        BaseOnButtonClicked(spawnButton);
+        battleManager = FindObjectOfType<BattleManagerDiwata>();
 
         if (spawnButton.name == "SpawnType1")
         {
             spawn1DestroyedCount++;
-            BattleManagerDiwata.Singleton.UpdateSigbinCount(spawn1DestroyedCount);
+            battleManager.UpdateSigbinCount(spawn1DestroyedCount);
         }
         else if (spawnButton.name == "SpawnType2")
         {
             spawn2DestroyedCount++;
-            BattleManagerDiwata.Singleton.UpdateTikbalangCount(spawn2DestroyedCount);
+            battleManager.UpdateTikbalangCount(spawn2DestroyedCount);
         }
 
         if (spawn1DestroyedCount >= destroyThreshold && spawn2DestroyedCount >= destroyThreshold)
         {
-            BattleManagerDiwata.Singleton.ShowUltimateButton();
+            battleManager.ShowUltimateButton();
         }
     }
 
@@ -32,7 +31,7 @@ public class SpawnerBossDiwata : SpawnerSigbinTikbalang
     {
         spawn1DestroyedCount = 0;
         spawn2DestroyedCount = 0;
-        BattleManagerDiwata.Singleton.UpdateSigbinCount(0);
-        BattleManagerDiwata.Singleton.UpdateTikbalangCount(0);
+        battleManager.UpdateSigbinCount(0);
+        battleManager.UpdateTikbalangCount(0);
     }
 }
--- Assets/Scripts/Combat_Scripts/Combat_Spawner_Scripts/SpawnerSigbinTikbalang.cs
+++ Assets/Scripts/Combat_Scripts/Combat_Spawner_Scripts/SpawnerSigbinTikbalang.cs
@@ -7,30 +7,34 @@ public class SpawnerSigbinTikbalang : Spawner
     [SerializeField] protected int destroyThreshold = 5;
     protected int spawn1DestroyedCount = 0;
     protected int spawn2DestroyedCount = 0;
+    private BattleManagerSigbinTikbalang battleManager;
 
     public override void OnButtonClicked(GameObject spawnButton)
     {
-        AudioManager.Singleton.PlaySwordSoundEffect(clickCount);
-        clickCount++;
-        Destroy(spawnButton);
-        currentSpawns.Remove(spawnButton);
+        base.OnButtonClicked(spawnButton);
+        battleManager = FindObjectOfType<BattleManagerSigbinTikbalang>();
 
         if (spawnButton.name == "SpawnType1")
         {
             spawn1DestroyedCount++;
-            BattleManagerSigbinTikbalang.Singleton.UpdateSigbinCount(spawn1DestroyedCount);
+            battleManager.UpdateSigbinCount(spawn1DestroyedCount);
         }
         else if (spawnButton.name == "SpawnType2")
         {
             spawn2DestroyedCount++;
-            BattleManagerSigbinTikbalang.Singleton.UpdateTikbalangCount(spawn2DestroyedCount);
+            battleManager.UpdateTikbalangCount(spawn2DestroyedCount);
         }
 
         base.OnButtonClicked(spawnButton);
 
         if (spawn1DestroyedCount >= destroyThreshold && spawn2DestroyedCount >= destroyThreshold)
         {
-            BattleManagerSigbinTikbalang.Singleton.Defeated();
+            battleManager.Defeated();
         }
+    }
+
+    protected void BaseOnButtonClicked(GameObject spawnButton)
+    {
+        base.OnButtonClicked(spawnButton);
     }
 }
