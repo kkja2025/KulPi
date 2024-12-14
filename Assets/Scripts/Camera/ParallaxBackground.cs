803889f2829097ba5d5719e468cd910e741cf057
Kert Solas <kertsolas@gmail.com>
2024-12-14 09:34:31 +0800

    parallax script update

--- Assets/Scripts/Camera/ParallaxBackground.cs
+++ Assets/Scripts/Camera/ParallaxBackground.cs
@@ -11,8 +11,8 @@ public class ParallaxBackground : MonoBehaviour
     public float parallaxEffectX;
     public float parallaxEffectY; // New variable for Y-axis parallax effect
 
-    public float minTriggerPoint;
-    public float maxTriggerPoint;
+    public float minTriggerPointX;
+    public float maxTriggerPointX;
 
     void Start()
     {
@@ -28,7 +28,9 @@ public class ParallaxBackground : MonoBehaviour
             float playerLocX = player.transform.position.x;
             float playerLocY = player.transform.position.y;
 
-            if (playerLocX >= minTriggerPoint && playerLocX <= maxTriggerPoint)
+            float limit = -104;
+
+            if (playerLocX >= minTriggerPointX && playerLocX <= maxTriggerPointX && playerLocY >= limit)
             {
                 float distanceX = playerLocX * parallaxEffectX;
                 float distanceY = playerLocY * parallaxEffectY;
@@ -45,7 +47,7 @@ public class ParallaxBackground : MonoBehaviour
                     startposX -= length;
                 }
             }
-            else if (playerLocX >= maxTriggerPoint)
+            else if (playerLocX >= maxTriggerPointX)
             {
                 return;
             }
