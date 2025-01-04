4b53d0e07d3dae7b39d0b6c75bd90d93eabc59c5
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2025-01-04 20:24:54 +0800

    location sound trigger

--- Assets/Scripts/Sound/LocationSoundTrigger.cs
+++ Assets/Scripts/Sound/LocationSoundTrigger.cs
@@ -3,15 +3,33 @@ using UnityEngine;
 public class LocationSoundTrigger : MonoBehaviour
 {
     [SerializeField] private AudioClip locationMusic;
+    private bool hasTriggered = false;
+
+    private void Update()
+    {
+        if (hasTriggered && locationMusic != null)
+        {
+            AudioManager.Singleton.PlayBackgroundSound(locationMusic, true);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !hasTriggered)
         {
+            hasTriggered = true;
             if (locationMusic != null)
             {
                 AudioManager.Singleton.PlayBackgroundSound(locationMusic, true);
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            hasTriggered = false;
         }
     }
 }
